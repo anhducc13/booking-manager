86e6814e639265629e3e9fd1b6f574b4dc8cd57e
ductt-teko <duc.tt@teko.vn>
2020-06-18 13:59:53 +0700

    update

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
   fixBabelImports('import', {
+    libraryName: 'antd',
     libraryDirectory: 'es',
     style: true,
   }),
